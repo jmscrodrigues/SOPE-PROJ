7b6de1d1c14f847810ba204296e39fcaaacb26eb
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2019-03-10 18:01:19 +0000

    ...

---
+++ BasicHandler.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define MAX_SIZE 100
+
+char BasicString(char *filename);
